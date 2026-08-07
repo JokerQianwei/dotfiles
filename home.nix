5d643d3b2e350f8c16283d9332b672f16a9fd565
JokerQianwei <72345663+JokerQianwei@users.noreply.github.com>
2026-08-07 23:24:53 +0800

    Back up tmux configuration

--- home.nix
+++ home.nix
@@ -102,6 +102,8 @@ in
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/nvim";
   home.file.".config/herdr".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/herdr";
+  home.file.".tmux.conf".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.tmux.conf";
   # Keep Pi's credential and runtime state local by linking only authored files and directories.
   home.file.".pi/agent/extensions".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.pi/agent/extensions";
