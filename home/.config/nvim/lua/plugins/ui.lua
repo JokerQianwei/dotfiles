cb6466e1c3b3b5582f5ff8216d4ccebcb0a018d9
JokerQianwei <72345663+JokerQianwei@users.noreply.github.com>
2026-08-07 22:46:09 +0800

    Add Markdown rendering to Neovim

--- home/.config/nvim/lua/plugins/ui.lua
+++ home/.config/nvim/lua/plugins/ui.lua
@@ -4,4 +4,16 @@ return {
     lazy = false,
     config = true,  -- popup that shows what my leader keys do
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = { 'markdown' },
+    dependencies = {
+      {
+        'nvim-treesitter/nvim-treesitter',
+        lazy = false,
+      },
+      'nvim-tree/nvim-web-devicons',
+    },
+    opts = {},
+  },
 }
