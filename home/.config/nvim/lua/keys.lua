3fe9809bb04d50fbf169de13b66ec6bbe004c7d6
JokerQianwei <72345663+JokerQianwei@users.noreply.github.com>
2026-08-09 16:33:07 +0800

    Map jk to Escape in Neovim

--- home/.config/nvim/lua/keys.lua
+++ home/.config/nvim/lua/keys.lua
@@ -1,3 +1,5 @@
+vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode' })
+
 -- select all
 vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'Select All' })
 -- pasting over a selection no longer clobbers your clipboard
