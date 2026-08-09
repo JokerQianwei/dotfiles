bac594d2e455030294cefb4f8e0c7047dae5d1a9
JokerQianwei <72345663+JokerQianwei@users.noreply.github.com>
2026-08-09 13:36:12 +0800

    Align Starship colors with Nord palette

--- home.nix
+++ home.nix
@@ -157,11 +157,40 @@ in
     settings = {
       add_newline = false;
       format = "$directory$git_branch$git_status$cmd_duration$line_break$character";
-      character = {
-        success_symbol = "[❯](purple)";
-        error_symbol = "[❯](red)";
+      palette = "nord";
+      palettes.nord = {
+        nord0 = "#2E3440";
+        nord1 = "#3B4252";
+        nord2 = "#434C5E";
+        nord3 = "#4C566A";
+        nord4 = "#D8DEE9";
+        nord5 = "#E5E9F0";
+        nord6 = "#ECEFF4";
+        nord7 = "#8FBCBB";
+        nord8 = "#88C0D0";
+        nord9 = "#81A1C1";
+        nord10 = "#5E81AC";
+        nord11 = "#BF616A";
+        nord12 = "#D08770";
+        nord13 = "#EBCB8B";
+        nord14 = "#A3BE8C";
+        nord15 = "#B48EAD";
       };
-      cmd_duration.format = "[$duration]($style) ";
+      directory = {
+        truncate_to_repo = false;
+        truncation_length = 0;
+        style = "bold nord8";
+      };
+      git_branch.style = "bold nord9";
+      git_status.style = "nord13";
+      character = {
+        success_symbol = "[❯](nord14)";
+        error_symbol = "[❯](nord11)";
+      };
+      cmd_duration = {
+        format = "[$duration]($style) ";
+        style = "nord3";
+      };
     };
   };
 
