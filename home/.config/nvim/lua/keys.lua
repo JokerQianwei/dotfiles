944b6dfc07464835ae069ab8f22141070eeaab9b
JokerQianwei <72345663+JokerQianwei@users.noreply.github.com>
2026-08-21 11:00:53 +0800

    Add previous buffer shortcut

--- home/.config/nvim/lua/keys.lua
+++ home/.config/nvim/lua/keys.lua
@@ -3,6 +3,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highl
 -- select all
 vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'Select All' })
 vim.keymap.set('n', '<leader>n', '<cmd>set number!<CR>', { desc = 'Toggle Line Numbers' })
+vim.keymap.set('n', '<leader><leader>', '<C-^>', { desc = 'Switch to Previous Buffer' })
 -- pasting over a selection no longer clobbers your clipboard
 vim.cmd([[ xnoremap <expr> p 'pgv"'.v:register.'y' ]])
 
